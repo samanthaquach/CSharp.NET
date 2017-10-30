90c7db3484ed457044e08aa2488496cb71261cda
samq1 <samanthatquach@gmail.com>
2017-10-30 13:29:30 -0500

    Added Files

---
+++ Collections_Practice/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayOfInts = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            string[] myCars = { "Tim", "Martin", "Nikki", "Sara" };
+
+            bool[] array = new bool[10];
+            array [0] = true;
+            array [2] = true;
+            array [4] = true;
+            array [6] = true;
+            array [8] = true;
+            foreach (bool value in array)
+            {
+                Console.WriteLine(value);
+            }
+            for (int i = 0; i < array.Length; i += 2)
+            {
+                array[i] = true;
+            }
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.Write(i + "\t");
+                for (int j = 1; j <= 10; j++)
+                {
+                    if (i > 0) Console.Write(i * j + "\t");
+                    else Console.Write(j + "\t");
+                }
+                Console.Write("\n");
+            }
+            List<string> icecream = new List<string>();
+            icecream.Add("vanilla");
+            icecream.Add("matcha");
+            icecream.Add("chocolate");
+            icecream.Add("lemon");
+            icecream.Add("strawberry");
+            Console.WriteLine(icecream.Count);
+            Console.WriteLine(icecream[3]);
+            icecream.Remove("lemon");
+            Console.WriteLine(icecream.Count);
+
+            Dictionary<string, string> profile = new Dictionary<string, string>();
+            profile.Add("Tim", "Vanilla");
+            profile.Add("Martin", "Matcha");
+            profile.Add("Nikki", "Chocolate");
+            profile.Add("Sara", "Lemon");
+            foreach (KeyValuePair<string, string> entry in profile)
+            {
+                Console.WriteLine(entry.Key + " - " + entry.Value);
+            }
+        }
+
+    }
+}
---
+++ Fundamentals_1/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace Fundamentals_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            for (int num = 1; num < 256; num++)
+            {
+                Console.WriteLine(num);
+            }
+
+            for (int num = 1; num < 101; num++)
+            {
+                if (!(num % 15 == 0))
+                {
+                    if (num % 3 == 0 || num % 5 == 0)
+                    {
+                        Console.WriteLine(num);
+                    }
+                }
+            }
+
+            for (int num = 1; num < 101; num++)
+            {
+                if (num % 3 == 0 && num % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (num % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (num % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+            }
+            // Optional
+            int three = 3;
+            int five = 5;
+            for (int num = 1; num < 101; num++)
+            {
+                three--;
+                five--;
+                if (three == 0 && five == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                    three = 3;
+                    five = 5;
+                }
+                else if (three == 0)
+                {
+                    Console.WriteLine("Fizz");
+                    three = 3;
+                }
+                else if (five == 0)
+                {
+                    Console.WriteLine("Buzz");
+                    five = 5;
+                }
+            }
+
+            // Optional 2
+            Random rand = new Random();
+            for (int num = 0; num <= 10; num++)
+            {
+                int val = rand.Next(1, 100);
+                string output = "For attempt " + num + " the value is " + val + " and the word is ";
+
+                if (val % 3 == 0 && val % 5 == 0)
+                {
+                    output += "FizzBuzz";
+                }
+                else if (val % 3 == 0)
+                {
+                    output += "Fizz";
+                }
+                else if (val % 5 == 0)
+                {
+                    output += "Buzz";
+                }
+                else {
+                    output += "Neither";
+                }
+                Console.WriteLine(output);
+            }
+
+
+        }
+
+    }
+}
+
