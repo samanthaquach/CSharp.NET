ec8ab643dc86eaf7521f00f9a8786e5e3b2b6ced
samq1 <samanthatquach@gmail.com>
2017-11-01 19:57:30 -0500

    CRUD and Music Linq

---
+++ CRUD/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using DbConnection;
+// using MySql.Data.MySqlClient;
+// using MySql.Data;
+using System.Collections.Generic;
+// using System.Data;
+
+namespace CRUD
+
+{
+    class Program
+    {
+        public static void ReadData()
+        {
+            // ------------------- Showing Users: 'x' is each user in Users --------------
+
+            var Users = DbConnector.Query("SELECT * FROM USERS ");
+            foreach (var x in Users)
+            {
+                Console.WriteLine("{0,9} {1,9} {2,9}", x["FirstName"],x["LastName"],x["FavoriteNumber"]);
+            }
+            
+
+
+        }
+        // ---------- Creating Data ----------
+
+        public static void CreateData()
+        {
+            Console.WriteLine("Enter First Name: ");
+            string first = Console.ReadLine();
+            Console.WriteLine("Enter Last Name: ");
+            string last = Console.ReadLine();
+            Console.WriteLine("Enter Favorite Number: ");
+            int FavoriteNumber = Convert.ToInt32(Console.ReadLine());
+            
+            try
+            {
+                DbConnector.Execute($"INSERT INTO Users (FirstName, LastName, FavoriteNumber)Values('{first}', '{last}', {FavoriteNumber})");
+                Console.WriteLine(" ========= You have successfully added! ========");
+            }
+            
+            catch
+            {
+                Console.WriteLine("======== I N V A L I D ! =========");
+            }
+
+        }
+
+        // ------------------- UPDATE ------------------
+        public static void UpdateData()
+        {   
+            Console.WriteLine("Enter User ID: ");
+            int userID = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter First Name: ");
+            string first = Console.ReadLine();
+            Console.WriteLine("Enter Last Name: ");
+            string last = Console.ReadLine();
+            Console.WriteLine("Enter Favorite Number: ");
+            int FavoriteNumber = Convert.ToInt32(Console.ReadLine());
+
+        DbConnector.Execute($"UPDATE Users SET FirstName='{first}', LastName='{last}', FavoriteNumber='{FavoriteNumber}' WHERE idUsers = {userID}; ");
+        }
+
+        // ---------------- Destroy --------------------
+        public static void DestroyData()
+        {
+            Console.WriteLine("Enter User ID: ");
+            try
+            {
+
+            int userID = Convert.ToInt32(Console.ReadLine());
+     
+            DbConnector.Execute($" DELETE FROM consoleDB.Users WHERE idUsers = {userID}; ");
+            Console.WriteLine("========= Delete Success =========");
+            }
+
+            catch
+            {
+                Console.WriteLine("You did not enter a Valid ID");
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            // ---------------- DISPLAYING ALL CRUD OPERATIONS --------------
+            
+            // CreateData();
+            UpdateData();
+            // DestroyData();
+            ReadData();
+        }
+
+
+
+    
+    }
+
+}
---
+++ Music_Linq/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using JsonData;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Collections to work with
+            List<Artist> Artists = JsonToFile<Artist>.ReadJson();
+            List<Group> Groups = JsonToFile<Group>.ReadJson();
+
+            //========================================================
+            //Solve all of the prompts below using various LINQ queries
+            //========================================================
+
+            //There is only one artist in this collection from Mount Vernon, what is their name and age?
+            // var results = from Artist in Artists where Artist.Hometown.Contains("Mount Vernon") select Artist;
+            // foreach (var artist in results)
+            // {
+            //     Console.WriteLine($"The artist is {artist.Age}. This artist is {artist.ArtistName}.");
+            // }
+
+            //Who is the youngest artist in our collection of artists?
+
+            // var youngest = Artists.OrderBy(x => x.Age).FirstOrDefault();
+            // System.Console.WriteLine($"This artist is {youngest.ArtistName}, and he is {youngest.Age}.");
+
+
+            //Display all artists with 'William' somewhere in their real name
+            // IEnumerable<Artist> Willy = Artists.Where(artist => artist.RealName.Contains("William"));
+            // foreach (var artist in Willy)
+            // {
+            //     Console.WriteLine($"{artist.ArtistName}");
+            // }
+            // List<Artist> Williams = Artists.Where(artist => artist.RealName.Contains("William")).ToList();
+            // Console.WriteLine("=============================");
+            // foreach (var artist in Williams)
+            // {
+            //     Console.WriteLine(artist.ArtistName + " - " + artist.RealName);
+            // }
+
+            // var billy = from Artist in Artists where Artist.RealName.Contains("William") select Artist;
+            // foreach (var artist in billy)
+            // {
+            //     Console.WriteLine(artist.ArtistName);
+            // }
+
+            //Display the 3 oldest artist from Atlanta
+            // IEnumerable<Artist> atlanta = Artists.OrderByDescending(x => x.Age).Take(3);
+            // foreach (var artist in atlanta)
+            // {
+            //     Console.WriteLine($"{artist.ArtistName} is the oldest artist.");
+            // }
+            // var atlanta = Artists.OrderByDescending(x => x.Age).Take(3);
+            // foreach (var artist in atlanta)
+            // {
+            //     Console.WriteLine($"These oldest artists is" +" " +artist.ArtistName);
+            // }
+
+            //(Optional) Display the Group Name of all groups that have members that are not from New York City
+            
+            // List<string> NonNewYorkGroups = Artists
+            //                     .Join(Groups, artist => artist.GroupId, group => group.Id, (artist, group) => { artist.Group = group; return artist; })
+            //                     .Where(artist => (artist.Hometown != "New York City" && artist.Group != null))
+            //                     .Select(artist => artist.Group.GroupName)
+            //                     .Distinct()
+            //                     .ToList();
+            // Console.WriteLine("All groups with members not from New York City:");
+            // foreach (var group in NonNewYorkGroups)
+            // {
+            //     Console.WriteLine(group);
+            // }
+
+            //(Optional) Display the artist names of all members of the group 'Wu-Tang Clan'
+            var Wu = Groups.Where(x => x.GroupName == "Wu-Tang Clan")
+                                    .GroupJoin(Artists, x => x.Id, 
+                                                        artist => artist.GroupId, 
+                                                        (x, artists) => 
+                                                        { x.Members = artists.ToList(); return x; })
+                                    .Single();
+            Console.WriteLine("===============================================");
+            foreach (var artist in Wu.Members)
+            {
+                Console.WriteLine(artist.ArtistName);
+            }
+
+        }
+    }
+}
